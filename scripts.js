823c8ce0d3c0d330991d3774d06dd274127f74f9
Gabriel Sales <kleythongabriel@outlook.com>
2024-10-05 14:04:30 -0300

    Atualização dos Compradores

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ fetch('numeros.json')
     .then(response => response.json())
     .then(data => {
         let comprados = 0;
+        let numPagos = []
         data.forEach(n => {
             if (n.comprador != '') {
                 numeros.innerHTML += `<div class="numero comprado flex-ac-jc"><p>${n.id}</p></div>`;
@@ -11,8 +12,14 @@ fetch('numeros.json')
             } else {
                 numeros.innerHTML += `<div class="numero flex-ac-jc"><p>${n.id}</p></div>`;
             }
+
+            if (n.formaPagamento != '') {
+                numPagos.push(n.comprador);
+            }
         });
 
         console.log(`Comprados: ` + comprados);
         console.log(`Não comprados: ` + (data.length - comprados));
+        console.log(`Compradores: ` + numPagos);
+        
     });

532b66d583bfeb52a87a6048d03cb0aa897525bb
Gabriel Sales <kleythongabriel@outlook.com>
2024-10-05 18:57:31 -0300

    Dashboard 0.0.1

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
-const numComprados = document.querySelector('#quantidade-numeros-disponiveis');
-const numDisponiveis = document.querySelector('#quantidade-numeros-comprados');
+const numDisponiveis = document.querySelector('#quantidade-numeros-disponiveis');
+const numComprados = document.querySelector('#quantidade-numeros-comprados');
+const faturamento = document.querySelector('#faturamento');
+const lucroPrevisto = document.querySelector('#lucro-previsto');
+const lucroReal = document.querySelector('#lucro-real');
+const numPagos = document.querySelector('#pagos');
+
+const pagamentosDinheiro = document.querySelector('#pagamentos-dinheiro');
+const pagamentosPix = document.querySelector('#pagamentos-pix');
+
+const barraPagamentoDinheiro = document.querySelector('#barra-progresso-dinheiro');
+const barraPagamentoPix = document.querySelector('#barra-progresso-pix');
 
 fetch('numeros.json')
     .then(response => response.json())
@@ -7,6 +17,19 @@ fetch('numeros.json')
 
         let comprados = data.filter(n => n.comprador != '');
 
-        
+        numDisponiveis.innerHTML = data.length - comprados.length;
+        numComprados.innerHTML = comprados.length;
 
+        let pagamentos = comprados.filter(n => n.formaPagamento != '');
+
+        faturamento.innerHTML = comprados.length * 20
+        lucroPrevisto.innerHTML = (comprados.length * 20) - 250
+        lucroReal.innerHTML = (pagamentos.length * 20) - 250
+
+        numPagos.innerHTML = pagamentos.length
+        pagamentosDinheiro.innerHTML = pagamentos.filter(n => n.formaPagamento == 'Dinheiro').length
+        pagamentosPix.innerHTML = pagamentos.filter(n => n.formaPagamento == 'Pix').length
+
+        barraPagamentoDinheiro.style.width = (pagamentos.filter(n => n.formaPagamento == 'Dinheiro').length / pagamentos.length) * 100 + "%";
+        barraPagamentoPix.style.width = (pagamentos.filter(n => n.formaPagamento == 'Pix').length / pagamentos.length) * 100 + "%";
     });
